758509cf18d5c7ea6b5f088d5f12f68ba9e4cc9d
webtactics <paul@webtactics.com.au>
2023-06-08 09:51:26 +1000

    Update script.js

--- _includes/assets/js/script.js
+++ _includes/assets/js/script.js
@@ -52,3 +52,33 @@ document.addEventListener('touchstart', onTouchStart, {passive: true});
 searchBox({
   container: document.querySelector('#searchbox'),
 });
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closed")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
