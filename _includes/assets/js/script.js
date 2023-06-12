4f92ea7d1c3d71b60eae426d22d6805bf5ca3947
webtactics <paul@webtactics.com.au>
2023-06-12 10:30:14 +1000

    4

--- _includes/assets/js/script.js
+++ _includes/assets/js/script.js
@@ -50,46 +50,16 @@ document.addEventListener('touchstart', onTouchStart, {passive: true});
 // Mobile menu toggle
 
 searchBox({
-  container: document.querySelector('#searchbox'),
+  container: document.querySelector('#search'),
 });
 
 // Accordion
 
-const accordion = document.querySelector(".accordion");
 
-accordion.addEventListener("click", (e) => {
-  const activePanel = e.target.closest(".accordion-panel");
-  if (!activePanel) return;
-  toggleAccordion(activePanel);
-});
-
-function toggleAccordion(panelToActivate) {
-  const activeButton = panelToActivate.querySelector("button");
-  const activePanel = panelToActivate.querySelector(".accordion-content");
-  const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
-
-  if (activePanelIsOpened === "true") {
-    panelToActivate
-      .querySelector("button")
-      .setAttribute("aria-expanded", false);
-
-    panelToActivate
-      .querySelector(".accordion-content")
-      .setAttribute("aria-hidden", true);
-  } else {
-    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
-
-    panelToActivate
-      .querySelector(".accordion-content")
-      .setAttribute("aria-hidden", false);
-  }
-}
 
 // toggle
 
 
 
-toggle.onclick = () => {
-  content.classList.toggle("expanded");
-};
+
 
