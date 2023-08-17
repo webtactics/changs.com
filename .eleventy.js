d1f370125e245445b36f2a0d06381a7fd955f935
webtactics <paul@webtactics.com.au>
2023-08-18 09:23:39 +1000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -75,33 +75,25 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
-    // add metagen plugin
-  const metagen = require('eleventy-plugin-metagen');
-
-module.exports = (eleventyConfig) => {
-    eleventyConfig.addPlugin(metagen);
-};
-
-    // add EleventyRenderPlugin plugin
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyRenderPlugin);
-};
 
 
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("site.webmanifest");
+  eleventyConfig.addPassthroughCopy({ static: "/" });
   eleventyConfig.addPassthroughCopy("admin/");
+  eleventyConfig.addPassthroughCopy("_redirects.txt");
   // We additionally output a copy of our CSS for use in Netlify CMS previews
-  eleventyConfig.addPassthroughCopy("_includes/assets/css/changs.css");
+  eleventyConfig.addPassthroughCopy("_includes/assets/css/jts-grid.css");
     // We additionally output a copy of our js for use in Netlify CMS previews
-    eleventyConfig.addPassthroughCopy("_includes/assets/js/script.js");
-  module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("script.js");
-  };
+  eleventyConfig.addPassthroughCopy("_includes/assets/js/script.js");
+
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
@@ -111,6 +103,7 @@ module.exports = function(eleventyConfig) {
     linkify: true,
     html: true,
   };
+
   let opts = {
     permalink: false
   };
