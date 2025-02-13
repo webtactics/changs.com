6d12eea9aedfbd47f86d4b166862c6a1d0133b26
Webtactics <paul@webtactics.com.au>
2025-02-13 11:12:19 +1100

    remove

--- _data/auth0.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  domain: "dev-a4aokb5vupf7v46t.us.auth0.com",
-  clientId: "V8XcVNN1qzavdg2N0FMVybxiPmY6C1an",
-  audience: "YOUR_AUTH0_AUDIENCE", // Optional - needed if calling an API
-  redirectUri: "https://changs.netlify.app//callback"
-};
